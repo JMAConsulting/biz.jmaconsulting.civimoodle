daf0e9541c7b2b3c5c4253b5ab01f13fe2ade9cf
deb.monish <deb.monish@gmail.com>
2017-03-08 22:38:41 +0530

    add setting form

---
+++ CRM/Civimoodle/Form/Setting.php
@@ -0,0 +1,93 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+
+/**
+ * Form controller class
+ *
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC43/QuickForm+Reference
+ */
+class CRM_Civimoodle_Form_Setting extends CRM_Core_Form {
+
+  /**
+   * CiviCRM Event IDs used for participant enrollment
+   *
+   * @var string
+   */
+  protected $_eventIDs;
+
+  /**
+   * Moodle Web Access token
+   *
+   * @var string
+   */
+  protected $_accessToken;
+
+  /**
+   * Moodle domain URL
+   *
+   * @var string
+   */
+  protected $_url;
+
+  /**
+   * Set variables up before form is built.
+   */
+  public function preProcess() {
+    if (!CRM_Core_Permission::check('administer CiviCRM')) {
+      CRM_Core_Error::fatal(ts('You do not permission to access this page, please contact your system administrator.'));
+    }
+    $this->_eventIDs = json_decode(Civi::settings()->get('moodle_events'), TRUE);
+    $this->_accessToken = Civi::settings()->get('moodle_access_token');
+    $this->_url = Civi::settings()->get('moodle_domain');
+  }
+
+  /**
+   * Set default values.
+   *
+   * @return array
+   */
+  public function setDefaultValues() {
+    $defaults = array(
+      'moodle_events' => $this->_eventIDs,
+      'moodle_access_token' => $this->_accessToken,
+      'moodle_domain' => $this->_url,
+    );
+    return $defaults;
+  }
+
+  public function buildQuickForm() {
+    $this->addEntityRef('moodle_events', ts('CiviCRM Events'), array(
+        'entity' => 'event',
+        'placeholder' => ts('- any -'),
+        'multiple' => 1,
+        'select' => array('minimumInputLength' => 0),
+      ),
+      TRUE
+    );
+    $this->add('password', 'moodle_access_token', ts('Moodle Web-access Token'), array('class' => 'huge'), TRUE);
+    $this->add('text', 'moodle_domain', ts('Moodle domain'), array('class' => 'huge'), TRUE);
+    $this->assign('moodleFields', array('moodle_events', 'moodle_access_token', 'moodle_domain'));
+
+    $this->addButtons(array(
+      array(
+        'type' => 'submit',
+        'name' => ts('Submit'),
+        'isDefault' => TRUE,
+      ),
+    ));
+
+    parent::buildQuickForm();
+  }
+
+  public function postProcess() {
+    $values = $this->exportValues();
+
+    Civi::settings()->set('moodle_events', json_encode(CRM_Utils_Array::value('moodle_events', $values)));
+    Civi::settings()->set('moodle_access_token', CRM_Utils_Array::value('moodle_access_token', $values));
+    Civi::settings()->set('moodle_domain', CRM_Utils_Array::value('moodle_domain', $values));
+
+    CRM_Core_Session::setStatus(ts("Moodle Settings submitted"), ts('Success'), 'success');
+  }
+
+}
